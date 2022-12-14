5298f2faf74f8a070507089a149e62ce97a1b05d
Teshi Waruingi <71100831+teshiwaruingi@users.noreply.github.com>
2022-12-13 19:37:06 -0500

    Update func-ptr.c

--- task1/func-ptr.c
+++ task1/func-ptr.c
@@ -25,97 +25,109 @@ typedef int (*Comparer) (const void *a, const void *b);
 int my_comparerforPriority(const void *this, const void *that)
 {
 	//TODO: IMPLEMENT ME!
+  int compare(const void *this, const void *that) {
+  // Compare the priorities of the two processes.
   if ((*(Process *)this).priority <(*(Process *)that).priority){
-    return 1;
+    return 1; // Return 1 if the first process has a higher priority.
   }
   if ((*(Process *)this).priority > (*(Process *)that).priority){
-    return -1;
+    return -1; // Return -1 if the second process has a higher priority.
   }
+  // If the priorities are the same, compare the arrival times of the processes.
   if ((*(Process *)this).arrival_time < (*(Process *)that).arrival_time){
-    return -1;
+    return -1; // Return -1 if the first process arrived earlier.
   }
-	else {
-    return 1;
+  else {
+    return 1; // Return 1 if the second process arrived earlier or at the same time.
   }
-  return 0;
+  return 0; // If none of the previous conditions are met, return 0.
 }
 
-int my_comparerforArrivalTime(const void *this, const void *that)
-{
-	 if ((*(Process *)this).arrival_time <(*(Process *)that).arrival_time){
-    return -1;
+
+int my_comparerforArrivalTime(const void *this, const void *that) {
+  // Compare the arrival times of the two processes.
+  if ((*(Process *)this).arrival_time <(*(Process *)that).arrival_time){
+    return -1; // Return -1 if the first process arrived earlier.
   }
   if ((*(Process *)this).arrival_time >(*(Process *)that).arrival_time){
+    return 1; // Return 1 if the second process arrived earlier.
+  }
+  return 0; // If the arrival times are the same, return 0.
+}
+
+int main(int argc, char *argv[]) {
+  // Check if the input file path was provided as a command line argument.
+  if (argc < 2) {
+    fprintf(stderr, "Usage: ./func-ptr <input-file-path>\n");
+    fflush(stdout);
     return 1;
   }
-	return 0;
-  
 }
 
 
-int main(int argc, char *argv[])
-{
-
-	if (argc < 2) {
-		   fprintf(stderr, "Usage: ./func-ptr <input-file-path>\n");
-		   fflush(stdout);
-		   return 1;
-	}
-
 	/*******************/
-	/* Parse the input */
-	/*******************/
-	FILE *input_file = fopen(argv[1], "r");
-	if (!input_file) {
-		   fprintf(stderr, "Error: Invalid filepath\n");
-		   fflush(stdout);
-		   return 1;
-	}
+/* Parse the input */
+/*******************/
+FILE *input_file = fopen(argv[1], "r");
+if (!input_file) {
+  // Check if the input file was successfully opened.
+  fprintf(stderr, "Error: Invalid filepath\n");
+  fflush(stdout);
+  return 1;
+}
 
-	Process *processes = parse_file(input_file);
+// Parse the input file and store the process information in an array of Process structs.
+Process *processes = parse_file(input_file);
 
-	/*******************/
-	/* sort the input  */
-	/*******************/
-	Comparer process_comparerforPriority = &my_comparerforPriority;
-  Comparer process_comparerforArrivalTime = &my_comparerforArrivalTime;
-
+/*******************/
+/* sort the input  */
+/*******************/
+// Define function pointers for comparing processes by priority and arrival time.
+Comparer process_comparerforPriority = &my_comparerforPriority;
+Comparer process_comparerforArrivalTime = &my_comparerforArrivalTime;
 
 #if DEBUG
-	for (int i = 0; i < P_SIZE; i++) {
-		    printf("%d (%d, %d) ",
-				processes[i].pid,
-				processes[i].priority, processes[i].arrival_time);
-	}
-	printf("\n");
+  // Print the processes in the array, if debugging is enabled.
+  for (int i = 0; i < P_SIZE; i++) {
+    printf("%d (%d, %d) ",
+      processes[i].pid,
+      processes[i].priority, processes[i].arrival_time);
+  }
+  printf("\n");
 #endif
 
-	qsort(processes, P_SIZE, sizeof(Process), process_comparerforPriority);
+	// Sort the processes in the array using the my_comparerforPriority function.
+qsort(processes, P_SIZE, sizeof(Process), process_comparerforPriority);
 
-	/**************************/
-	/* print the sorted data  */
-	/**************************/
-  printf("sorted based on priority\n");
-	for (int i = 0; i < P_SIZE; i++) {
-		    printf("%d (%d, %d)\n",
-				processes[i].pid,
-				processes[i].priority, processes[i].arrival_time);
-	}
-  
-  qsort(processes, P_SIZE, sizeof(Process), process_comparerforArrivalTime);
-  printf("sorted based on arrival times\n");
-  for (int i = 0; i < P_SIZE; i++) {
-		    printf("%d (%d, %d)\n",
-				processes[i].pid,
-				processes[i].priority, processes[i].arrival_time);
-	}
-	fflush(stdout);
-	fflush(stderr);
+/**************************/
+/* print the sorted data  */
+/**************************/
+// Print the sorted processes based on their priorities.
+printf("sorted based on priority\n");
+for (int i = 0; i < P_SIZE; i++) {
+  printf("%d (%d, %d)\n",
+    processes[i].pid,
+    processes[i].priority, processes[i].arrival_time);
+}
 
-	/************/
-	/* clean up */
-	/************/
-	free(processes);
-	fclose(input_file);
+// Sort the processes in the array using the my_comparerforArrivalTime function.
+qsort(processes, P_SIZE, sizeof(Process), process_comparerforArrivalTime);
+
+// Print the sorted processes based on their arrival times.
+printf("sorted based on arrival times\n");
+for (int i = 0; i < P_SIZE; i++) {
+  printf("%d (%d, %d)\n",
+    processes[i].pid,
+    processes[i].priority, processes[i].arrival_time);
+}
+
+// Flush the standard output and error streams.
+fflush(stdout);
+fflush(stderr);
+
+/************/
+/* clean up */
+/************/
+
 	return 0;
 }
